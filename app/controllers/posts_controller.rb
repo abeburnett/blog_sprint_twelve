ec2c639f3ad9b903626fa271077de785d4fb08e5
Abraham R Burnett <abeburnett@gmail.com>
2014-09-24 16:13:28 -0700

    create and edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :text))
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params[:post].permit(:title, :text))
+    redirect_to @post
+  end
+
+  def index
+    @posts = Post.all
+  end
+
 end
