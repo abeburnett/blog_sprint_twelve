de90f4bfeab678f125ca99d333079a48a09c2acc
Abraham R Burnett <abeburnett@gmail.com>
2014-09-24 16:45:12 -0700

    slim down posts controller with before actioin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+
   def new
     @post = Post.new
   end
@@ -14,15 +16,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(params[:post].permit(:title, :text))
     redirect_to @post
   end
@@ -32,9 +31,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
+  end
+
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
